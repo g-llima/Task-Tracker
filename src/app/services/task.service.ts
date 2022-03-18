b4762adc0e82947dc935fcdd76876ca0472e9f75
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-18 15:13:18 -0300

    Switch to JSON Server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -13,7 +13,8 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks';
+  private apiUrl =
+    'https://my-json-server.typicode.com/g-llima/Task-Tracker/tasks';
 
   constructor(private http: HttpClient) {}
 
