763e7f9dcfceccbc84fe3df8b131c012f6a12e4b
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-18 17:27:51 -0300

    Change JSON URL

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -13,7 +13,7 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks';
+  private apiUrl = 'https://angular-tasktracker-app.herokuapp.com/tasks';
 
   constructor(private http: HttpClient) {}
 
